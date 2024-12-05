b9e98ac2bb6512f8151270cbf4809b9b4d1c4327
SergioAlvarezHerrera10 <sergio.alvaro@alumno.iesluisvives.org>
2024-12-05 20:00:14 +0100

    Initial commit

---
+++ ejercicio1.py
@@ -0,0 +1,20 @@
+
+
+print(type(print))
+print(type(1))
+print(type(1.2))
+print(type("hola"))
+print(type(True))
+
+name = "sergio"
+print (type(name))
+
+name = 30
+
+pirint (type(name))
+
+id = input("que id desea añadir")
+print(id)
+
+adress = " calle de la fantasia n13 4D"
+print(adress)
